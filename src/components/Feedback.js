14b6bc02006d314a060235ffdfc6c6f19c282b2c
Llewellyn Falco <isidore@setgame.com>
2017-02-08 22:28:52 +0200

    smileys

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -7,7 +7,7 @@ class Feedback extends React.Component {
 
     var room = document.getElementById("room").value;
     var feedback = { room: room, rating: rating, time: new Date()}
-    
+
     this.props.submit( feedback );
   }
 
@@ -22,10 +22,10 @@ class Feedback extends React.Component {
               <option selected value="C">C</option>
               <option value="D">D</option>
             </select>
-      
-            <button onClick={() => this.submitFeedback(5)}> <img  src="http://www.clker.com/cliparts/d/E/Y/K/Q/E/happy-green-face-md.png"/></button>
-            <button onClick={() => this.submitFeedback(3)}>  <img src="http://www.clker.com/cliparts/e/8/3/a/11949864601483835659smiley103.svg.med.png"/></button>
-            <button onClick={() => this.submitFeedback(1)}> <img src="http://www.clker.com/cliparts/b/3/7/9/1206573862448734250Arnoud999_Right_or_wrong_3.svg.med.png"/> </button>
+
+            <button onClick={() => this.submitFeedback(5)}> <img  src="http://feedback.rinkkasatiainen.fi/Happy.png"/></button>
+            <button onClick={() => this.submitFeedback(3)}>  <img src="http://feedback.rinkkasatiainen.fi/Meh.png"/></button>
+            <button onClick={() => this.submitFeedback(1)}> <img src="http://feedback.rinkkasatiainen.fi/Sad.png/> </button>
 
         </div>
     )
