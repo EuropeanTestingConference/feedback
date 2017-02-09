219302568acdca812600ef6342fb2881d8fd62b0
Aki Salmi <aki.salmi@iki.fi>
2017-02-09 10:45:54 +0200

    animation on button

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -3,8 +3,38 @@ import React from 'react';
 class Feedback extends React.Component {
 
 
-  submitFeedback( rating ){
+  constructor (props) {
+    super(props)
+    this.state = {}
+    this.state.b5 = false
+    this.state.b3 = false
+    this.state.b1 = false
+    this.fadingDone = this.fadingDone.bind(this)
+  }
+  componentDidMount () {
+    const b5 = this.refs.b5
+    const b3 = this.refs.b3
+    const b1 = this.refs.b1
+    b5.addEventListener('animationend', () => this.fadingDone("b5"))
+    b3.addEventListener('animationend', () => this.fadingDone("b3"))
+    b1.addEventListener('animationend', () => this.fadingDone("b1"))
+  }
+  componentWillUnmount () {
+    const b5 = this.refs.b5
+    const b3 = this.refs.b3
+    const b1 = this.refs.b1
+    b5.removeEventListener('animationend', () => this.fadingDone("b5"))
+    b3.removeEventListener('animationend', () => this.fadingDone("b3"))
+    b1.removeEventListener('animationend', () => this.fadingDone("b1"))
+  }
 
+  fadingDone( button ){
+    console.log( button )
+    this.setState({[button]: false})
+  }
+
+  submitFeedback( rating , button){
+    this.setState({[button]: true})
     var room = document.getElementById("room").value;
     var time = new Date()
     var feedback = { room: room, rating: rating, time: time.toString() }
@@ -31,9 +61,9 @@ class Feedback extends React.Component {
             <br/>
 
             <div style={{widht:"100%"}}>
-            <button style={{"maxWidth": "30%"}} onClick={() => this.submitFeedback(1)}> <img style={{width:"100%"}} src="http://feedback.rinkkasatiainen.fi/Sad.png"/> </button>
-            <button style={{"maxWidth": "30%"}} onClick={() => this.submitFeedback(3)}>  <img style={{width:"100%"}} src="http://feedback.rinkkasatiainen.fi/Meh.png"/></button>
-            <button style={{"maxWidth": "30%"}} onClick={() => this.submitFeedback(5)}> <img style={{width:"100%"}} src="http://feedback.rinkkasatiainen.fi/Happy.png"/></button>
+            <button ref='b1' style={{"maxWidth": "30%"}} className={this.state.b1 ? 'b1' : ''}  onClick={() => this.submitFeedback(1, "b1")}> <img style={{width:"100%"}} src="http://feedback.rinkkasatiainen.fi/Sad.png"/> </button>
+            <button ref='b3' style={{"maxWidth": "30%"}} className={this.state.b3 ? 'b3' : ''} onClick={() => this.submitFeedback(3, "b3")}>  <img style={{width:"100%"}} src="http://feedback.rinkkasatiainen.fi/Meh.png"/></button>
+            <button ref='b5' style={{"maxWidth": "30%"}} className={this.state.b5 ? 'b5' : ''} onClick={() => this.submitFeedback(5, "b5")}> <img style={{width:"100%"}} src="http://feedback.rinkkasatiainen.fi/Happy.png"/></button>
 
           </div>
         </div>
